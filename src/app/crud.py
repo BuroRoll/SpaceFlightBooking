c12956dec80a85acf52ea43604cd5819253f8074
Danil <BuroRoll@gmail.com>
2024-06-26 16:57:21 +0500

    Update crud.py

--- src/app/crud.py
+++ src/app/crud.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
-from models import *
-from schemas import *
+
+import models
+import schemas
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -14,9 +15,9 @@ def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
 
-def create_user(db: Session, user: UserCreate):
+def create_user(db: Session, user: schemas.UserCreate):
     hashed_password = get_password_hash(user.password)
-    db_user = User(
+    db_user = models.User(
         email=user.email,
         hashed_password=hashed_password,
         name=user.name,
@@ -29,11 +30,11 @@ def create_user(db: Session, user: UserCreate):
 
 
 def get_user_by_email(db: Session, email: str):
-    return db.query(User).filter(User.email == email).first()
+    return db.query(models.User).filter(models.User.email == email).first()
 
 
-def create_booking(db: Session, booking: BookingCreate, user_id: int):
-    db_booking = Booking(**booking.dict(), user_id=user_id)
+def create_booking(db: Session, booking: schemas.BookingCreate, user_id: int):
+    db_booking = models.Booking(**booking.dict(), user_id=user_id)
     db.add(db_booking)
     db.commit()
     db.refresh(db_booking)
@@ -41,4 +42,4 @@ def create_booking(db: Session, booking: BookingCreate, user_id: int):
 
 
 def get_user_bookings(db: Session, user_id: int):
-    return db.query(Booking).filter(Booking.user_id == user_id).all()
+    return db.query(models.Booking).filter(models.Booking.user_id == user_id).all()
