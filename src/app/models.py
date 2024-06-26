832a683406c0f33912baf26ff0139032c90d971f
Danil <BuroRoll@gmail.com>
2024-06-26 16:38:35 +0500

    Update models.py

--- src/app/models.py
+++ src/app/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from sqlalchemy.sql.sqltypes import DateTime
-from .database import Base
+from database import Base
 
 
 class User(Base):
