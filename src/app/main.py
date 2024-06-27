b1004f1092ad5b78bf318f5dcceafc7f3797e5c9
Danil <BuroRoll@gmail.com>
2024-06-27 16:18:14 +0500

    Update main.py

--- src/app/main.py
+++ src/app/main.py
@@ -18,13 +18,13 @@ app.include_router(booking_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=['*'],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "DELETE", "OPTIONS"],
-    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers",
-                   "Access-Control-Allow-Origin",
-                   "Authorization", "Origin", "Accept"],
+    allow_methods=['GET', 'POST', 'DELETE', 'OPTIONS', 'PUT'],
+    allow_headers=['Content-Type', 'Set-Cookie', 'Access-Control-Allow-Headers',
+                   'Access-Control-Allow-Origin',
+                   'Authorization', 'Origin', 'Accept'],
 )
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True)
